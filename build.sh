fc7d89f83ab1a9324c3731a43631860623413dd1
sulincix <parduscix@yandex.ru>
2023-12-14 08:02:46 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -24,7 +24,7 @@ if which apt &>/dev/null && [[ -d /var/lib/dpkg && -d /etc/apt ]] ; then
 fi
 set -ex
 rm -rf grub-os isowork grub-os-install.iso || true
-debootstrap --variant=minbase --arch=amd64 --no-check-gpg --no-merged-usr testing grub-os
+debootstrap --variant=minbase --arch=amd64 --no-check-gpg --no-merged-usr sid grub-os
 chroot grub-os apt install grub-pc-bin grub-efi grub-common os-prober ntfs-3g efibootmgr zstd -y
 chroot grub-os apt install linux-image-amd64 --no-install-recommends live-boot -y
 rm -rf grub-os/lib/modules/*/kernel/drivers/gpu
